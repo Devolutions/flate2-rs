369866e6e4bbf342da123779a3b88874018300f1
Marc-André Moreau <marcandre.moreau@gmail.com>
2019-05-28 09:30:41 -0400

    miniz-sys: use conan package

--- miniz-sys/build.rs
+++ miniz-sys/build.rs
@@ -1,8 +1,28 @@
 extern crate cc;
 
 use std::env;
+use std::path::Path;
+
+extern crate conan;
+use conan::*;
 
 fn main() {
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let conan_profile = format!("{}-{}", target_os, target_arch);
+
+    let command = InstallCommandBuilder::new()
+        .with_profile(&conan_profile)
+        .build_policy(BuildPolicy::Never)
+        .recipe_path(Path::new("conanfile.txt"))
+        .build();
+
+    if let Some(build_info) = command.generate() {
+        println!("using conan build info");
+        build_info.cargo_emit();
+        return;
+    }
+    
     let target = env::var("TARGET").unwrap();
     if target.starts_with("wasm32-") && !target.ends_with("-emscripten") {
         return;
